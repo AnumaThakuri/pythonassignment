5d77a2158a7a99b6d56be848a0923984de423bfe
AnumaThakuri <anuma.thakuri555@gmail.com>
2021-02-09 00:24:33 +0530

    assignment1

---
+++ question 8.py
@@ -0,0 +1,12 @@
+C=float(input("enter the 1st number"))
+D=float(input("enter the second number"))
+
+print("before interchange")
+print("C:",C)
+print("D:",D)
+
+C,D=D,C
+
+print("after interchanging:")
+print("new C:",C)
+print("new D:",D) 
